39d41fae8cf9cdf7f89687dcf649aed7c8d2185e
s2ljeun <s2ljeun@gmail.com>
2022-07-11 23:44:59 +0900

    make components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import styled, {css} from 'styled-components';
+import { MdDone, MdDelete } from 'react-icons/md';
+
+const Remove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #dee2e6;
+  font-size: 24px;
+  cursor: pointer;
+  &:hover {
+    color: #ff6b6b;
+  }
+  display: none;
+`
+
+const TodoItemBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 12px;
+  padding-bottom: 12px;
+  &:hover {
+    ${Remove} {
+      display: initial;
+    }
+  }
+`;
+// Component Selector
+// TodoItemBlock가 hover상태일때, Remove 컴포넌트를 display하라는 의미
+
+const CheckCircle = styled.div`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+  border: 1px solid #ced4da;
+  font-size: 24px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+  cursor: pointer;
+  ${props =>
+    props.done &&
+    css`
+      border: 1px solid #38d9a9;
+      color: #38d9a9;
+    `}
+`;
+
+// styled component 활용
+//${props => props.넘긴prop이름 && css`넘어왔을때 적용할 css내용`}
+
+const Text = styled.div`
+  flex: 1;
+  font-size: 21px;
+  color: #495057;
+  ${props =>
+    props.done &&
+    css`
+      color: #ced4da;
+    `}
+`;
+
+function TodoItem({ id, done, text }){
+  return(
+    <TodoItemBlock>
+      <CheckCircle done={done}>{done && <MdDone />}</CheckCircle>
+      <Text done={done}>{text}</Text>
+      <Remove>
+        <MdDelete />
+      </Remove>
+    </TodoItemBlock>
+  );
+}
+
+export default TodoItem;
